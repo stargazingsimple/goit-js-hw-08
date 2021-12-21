ec335a0b213b737943bef156a175c3c586976fc5
stargazingsimple <stargazingsimple@gmail.com>
2021-12-21 20:07:15 +0200

    fix video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -20,19 +20,8 @@ player.on(
   }, 1000),
 );
 
-player
-  .setCurrentTime(localStorage.getItem(STORAGE_KEY))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+const saveTime = localStorage.getItem(STORAGE_KEY);
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+if (saveTime) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
+}
